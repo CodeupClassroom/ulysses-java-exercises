e15cbf80aa48bfef0542396011e8e568a2f97f04
Ryan Orsinger <ryan.orsinger@gmail.com>
2018-04-30 10:57:26 -0500

    First commit with .gitignore and HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * ClassName needs to match the filename
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+        System.out.println(); // An empty sout gives you a new line
+        System.out.println("The shortcut to make a main method is psvm followed by tab..");
+        System.out.println("The shortcut for System.out.println() is sout then a tab...");
+    }
+}
