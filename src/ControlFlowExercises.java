ea4c0822a7ff95fcd9ffe562b6e298a8e0d95684
Ryan Orsinger <ryan.orsinger@gmail.com>
2018-05-01 14:16:57 -0500

    Add grade solution for the control structure exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,6 +2,8 @@
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
 
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {  // global scope
         int /* type declaration */ i; // variable declaration, followed by an assignment
@@ -24,5 +26,61 @@ public class ControlFlowExercises {
             System.out.println(i);
         }
 
+        grades();
+    }
+
+    // Convert given number grades into letter grades.
+    public static void grades() {
+        Scanner input = new Scanner(System.in).useDelimiter("\n");
+        String userChoice;
+        short gradeNumber;
+        int gradeTotal = 0;
+        int numberOfGrades = 0;
+        int average;
+
+        do {
+            // Prompt the user for a numerical grade from 0 to 100.
+            System.out.print("Please input the student's grade between 0 and 100: ");
+            gradeNumber = input.nextShort();
+
+            String letterGrade = letterGrade(gradeNumber);
+
+            System.out.println("The letter grade for " + gradeNumber + " is : " + letterGrade);
+
+            numberOfGrades++;
+            gradeTotal += gradeNumber;
+
+            System.out.println("Do you want to calculate another grade?");
+            userChoice = input.next();
+
+        } while(userChoice.equalsIgnoreCase("y") || userChoice.equalsIgnoreCase("yes"));
+
+        average = gradeTotal / numberOfGrades;
+        System.out.println("The average grade for all assignments is: " + average);
+
+        System.out.println("The average letter grade is: " + letterGrade(average));
+
+    }
+
+    public static String letterGrade(int gradeNumber) {
+        String letterGrade;
+
+        if(gradeNumber >= 90 && gradeNumber <= 100) {
+            letterGrade = "A";
+        } else if(gradeNumber >= 80) {
+            letterGrade = "B";
+        } else if(gradeNumber >= 70) {
+            letterGrade = "C";
+        } else if(gradeNumber >= 60) {
+            letterGrade = "D";
+        } else if(gradeNumber >=0 && gradeNumber <= 59){
+            letterGrade = "F";
+        } else {
+            letterGrade = "This is not a valid grade. Please try again.";
+        }
+
+        return letterGrade;
     }
 }
+
+
