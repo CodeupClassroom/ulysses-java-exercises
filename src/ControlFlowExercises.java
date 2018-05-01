0f62d73324a6958d560d6dd693695a4e2c55bee9
Luis Montealegre <montealegreluis@gmail.com>
2018-05-01 11:32:47 -0500

    feat: Count from 5 to 15 using a while loop

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,18 @@
+/*
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {  // global scope
+        int i = 5; // variable declaration, followed by an assignment
+
+        while (
+            i <= 15 /* boolean expression -> the condition to stop */
+        ) {
+            System.out.println(i);
+            i++;
+//             i += 1;
+//             i = i + 1;
+        }
+    }
+}
