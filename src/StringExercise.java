81323dbd15274ea4429281a7022cac65b0c1ad0e
Ryan Orsinger <ryan.orsinger@gmail.com>
2018-05-01 15:10:36 -0500

    Add StringExercise solution with escape characters

---
+++ src/StringExercise.java
@@ -0,0 +1,11 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String message = "We don't need no education\n" +
+                "We don't need no thought control.\n" +
+                "check \"this\" out, \"s inside of \"s\n" +
+                "In windows, the main drive is C:\\\n" +
+                "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash, \\\\\\";
+        System.out.println(message);
+    }
+}
