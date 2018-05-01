777d099ad1b86f24b7edf827b9c0b3d940cb5ea7
Luis Montealegre <montealegreluis@gmail.com>
2018-05-01 11:37:30 -0500

    feat: count from 0 to 100 using a do while

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,7 +4,7 @@
 
 public class ControlFlowExercises {
     public static void main(String[] args) {  // global scope
-        int i = 5; // variable declaration, followed by an assignment
+        int /* type declaration */ i = 5; // variable declaration, followed by an assignment
 
         while (
             i <= 15 /* boolean expression -> the condition to stop */
@@ -14,5 +14,12 @@ public class ControlFlowExercises {
 //             i += 1;
 //             i = i + 1;
         }
+
+        i = 0;
+        do { // do while body
+            System.out.println(i);
+            i += 2;
+        } while (i <= 100 /* condition to continue*/);
+
     }
 }
