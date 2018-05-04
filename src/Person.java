0afe4f80cf01d0051e7d0e21520b6d6f78e9a6be
Luis Montealegre <montealegreluis@gmail.com>
2018-05-04 11:03:22 -0500

    chore: Add 3 new empty methods

--- src/Person.java
+++ src/Person.java
@@ -7,4 +7,19 @@ public class Person {
     // private name property that is a string
     // property /  field / attribute / instance variable
     private String name;
+
+    // returns the person's name
+    public String getName() {
+        return null;
+    }
+
+    // changes the name property to the passed value
+    public void setName(String name) {
+
+    }
+
+    // prints a message to the console using the person's name
+    public void sayHello() {
+
+    }
 }
