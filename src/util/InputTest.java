94245d20cbc9db85ab81ffd06e2853d95707b9d2
Luis Montealegre <montealegreluis@gmail.com>
2018-05-04 13:34:05 -0500

    chore: Create InputTest class with a main method

---
+++ src/util/InputTest.java
@@ -0,0 +1,10 @@
+/*
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+
+    }
+}
