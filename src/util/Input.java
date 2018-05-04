5b65c81218808436b9dfa9af2d356797bd7cf9ec
Luis Montealegre <montealegreluis@gmail.com>
2018-05-04 12:15:26 -0500

    chore: Add empty methods to Input

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,5 +3,35 @@
  */
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+    // option 1) Inline the assignment
+    // private Scanner scanner = new Scanner(System.in);  // = null
+    private  Scanner scanner;
+
+    // option 2 -> Create a constructor
+    public Input() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String getString() {
+        return null;
+    }
+
+    boolean yesNo() {
+        return true;
+    }
+    int getInt(int min, int max) {
+        return 0;
+    }
+    int getInt() {
+        return 0;
+    }
+    double getDouble(double min, double max) {
+        return 0;
+    }
+    double getDouble() {
+        return 0;
+    }
 }
