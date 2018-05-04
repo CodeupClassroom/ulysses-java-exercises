964c73e098197d9eaf8fca490cd3444334c3fe9d
Luis Montealegre <montealegreluis@gmail.com>
2018-05-04 11:01:10 -0500

    chore: Create private instance variable named 'name'

--- src/Person.java
+++ src/Person.java
@@ -4,4 +4,7 @@
 
 // Create Person class inside of src
 public class Person {
+    // private name property that is a string
+    // property /  field / attribute / instance variable
+    private String name;
 }
